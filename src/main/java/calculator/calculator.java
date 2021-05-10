3c1cac3346818c7be350889e88d7d0bcb145658f
bszczypior1993 <bszczypior@interia.pl>
2021-05-10 20:29:58 +0200

    Initial commit

---
+++ src/main/java/calculator/calculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class calculator {
+
+    public static int add(int firstValue, int secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public static int deduct(int firstValue, int secondValue) {
+        return firstValue - secondValue;
+    }
+
+    public static int multiply(int firstValue, int secondValue) {
+        return firstValue * secondValue;
+    }
+
+    public static int divide(int firstValue, int secondValue) {
+        return firstValue / secondValue;
+    }
+}
